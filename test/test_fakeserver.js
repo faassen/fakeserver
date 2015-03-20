105317871374847445463f40b825c492a713b5af
faassen <faassen@startifact.com>
2015-03-20 17:10:36 +0100

    Can return body directly instead of Response.

--- test/test_fakeserver.js
+++ test/test_fakeserver.js
@@ -19,6 +19,21 @@ suite("fakeserver", function() {
         });
         server.stop();
     })
+    test("responds to GET request with body directly", function(done) {
+        let server = new FakeServer();
+        function handler(variables, request) {
+            return JSON.stringify({'animal': variables.id});
+        }
+        server.register('animals/{id}', handler);
+
+        server.start();
+        $.getJSON('animals/chicken').done(function(value) {
+            assert.deepEqual({'animal': "chicken"}, value);
+            done();
+        });
+        server.stop();
+    })
+
     test("404", function(done) {
         let server = new FakeServer();
         function handler(variables, request) {
